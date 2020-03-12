978b804c8259338a8e5bb7da73aa6028de4bfc8c
Celine <celine.liu@hootsuite.com>
2020-03-12 15:49:14 -0700

    supporting fr

---
+++ README.md
@@ -0,0 +1,16 @@
+# Neural Translator
+Using pytorch pretrained fairseq 
+
+## Available language model:
+- French as fe
+- Germen as de
+
+## POST request
+Please follow format:
+> http://0a4e6a7e.ngrok.io/translate?msg=input_string&lang=target_langauge
+
+
+
+
+
+
---
+++ translator_post_multi.py
@@ -0,0 +1,27 @@
+import hug
+import torch
+import fairseq
+
+# List available models
+torch.hub.list('pytorch/fairseq')  # [..., 'transformer.wmt16.en-de', ... ]
+
+# Load a transformer trained on WMT'16 En-De
+en2de = torch.hub.load('pytorch/fairseq', 'transformer.wmt16.en-de', tokenizer='moses', bpe='subword_nmt')
+en2de.eval()  # disable dropout
+
+en2fr = torch.hub.load('pytorch/fairseq', 'conv.wmt14.en-fr', tokenizer='moses', bpe='subword_nmt')
+en2fr.eval()  # disable dropout
+
+# The underlying model is available under the *models* attribute
+#assert isinstance(en2de.models[0], fairseq.models.transformer.TransformerModel)
+
+
+
+@hug.post(examples='txt="Hello World"')
+@hug.local()
+def translate(msg: hug.types.text, lang: hug.types.text):
+	if lang == "fr":
+		model = en2fr
+	elif lang == "de":
+		model = en2de
+	return {'msg':model.translate(msg)}
