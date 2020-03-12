f0ee55e8d0ab48340d526b980f11b755d5698623
Celine <celine.liu@hootsuite.com>
2020-03-12 16:02:59 -0700

    ru

--- translator_post_multi.py
+++ translator_post_multi.py
@@ -12,6 +12,12 @@ en2de.eval()  # disable dropout
 en2fr = torch.hub.load('pytorch/fairseq', 'conv.wmt14.en-fr', tokenizer='moses', bpe='subword_nmt')
 en2fr.eval()  # disable dropout
 
+
+en2ru = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.en-ru', tokenizer='moses', bpe='subword_nmt')
+en2ru.eval()  # disable dropout
+
+
+
 # The underlying model is available under the *models* attribute
 #assert isinstance(en2de.models[0], fairseq.models.transformer.TransformerModel)
 
@@ -24,4 +30,6 @@ def translate(msg: hug.types.text, lang: hug.types.text):
 		model = en2fr
 	elif lang == "de":
 		model = en2de
+	elif lang == "ru"
+		model == "en2ru"
 	return {'msg':model.translate(msg)}
